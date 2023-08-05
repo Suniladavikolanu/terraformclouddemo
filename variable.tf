4d7ceb34cc5ce2173b48852597f68e239fdda34a
ramesh <rameshshkharat@gmail.com>
2023-08-05 13:13:43 +0530

    1

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "Region of AWS"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type "
+  type        = string
+}
+variable "access_key" {
+  type = string
+}
+variable "secret_key" {
+  type = string
+}
+
