8586ed67af80983012032dd8c9ebaa9078cfba99
Ramesh KHARAT <62638281+kharatramesh@users.noreply.github.com>
2023-08-06 09:51:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "vm1" {
     ami = "ami-0f34c5ae932e6f0e4"
     instance_type ="t2.micro"
-    tags = {
-        Name ="test1"
-    }
+#    tags = {
+ #       Name ="test1"
+  #  }
 }
