e0929476ae14037b1c01f61936b1f3c3cf685b20
Ramesh KHARAT <62638281+kharatramesh@users.noreply.github.com>
2023-08-08 14:59:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,1 @@
-resource "aws_instance" "vm1" {
-    ami = "ami-0ded8326293d3201b"
-    instance_type ="t2.small"
-    tags = {
-      Name ="test1"
-   }
-}
 
-terraform {
-  cloud {
-    organization = "priar"
-    ## Required for Terraform Enterprise; Defaults to app.terraform.io for Terraform Cloud
-    hostname = "app.terraform.io"
-
-    workspaces {
-      tags = ["terraformclouddemo"]
-    }
-  }
-}
