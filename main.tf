5461a5552812d113b2004835d5fb5e465881107c
Ramesh KHARAT <62638281+kharatramesh@users.noreply.github.com>
2023-08-08 15:03:21 +0530

    statecheck

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
+data "terraform_remote_state" "vadapav" {
+  backend = "remote"
+  config = {
+    organization = "hashicorp"
+    workspaces = {
+      name = "terraformwipro"
+    }
+  }
+}
 
+resource "aws_instance" "statecheck" {
+
+    ami = var.amiid
+    instance_type = var.instancetype
+    tags = {
+        Name = "statecheck"
+    }
+}
